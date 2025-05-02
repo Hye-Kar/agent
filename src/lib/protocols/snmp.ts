4b609f93f7cb1e7bffbf071c3826593d2e4d5c53
Robert de Leeuw <klokko06@gmail.com>
2025-05-02 20:17:39 +0200

    KAR-15 Add config to SNMP

--- src/lib/protocols/snmp.ts
+++ src/lib/protocols/snmp.ts
@@ -1,12 +1,23 @@
 import snmp from "net-snmp"
+import parseConfig, { ParsedConfig, RawConfig, satisfiesConfig } from "../config"
 
 class Client {
     session: snmp.Session
-    constructor(options: {
-        host: string,
-        community?: string
-    }) {
-        this.session = snmp.createSession(options.host, options.community ?? "public")
+    config: ParsedConfig
+    constructor(config: RawConfig) {
+        if (!satisfiesConfig([
+            "snmp.host"
+        ], config)) {
+            throw new Error("Invalid config")
+        }
+
+        this.config = parseConfig(config);
+        
+        if (this.config.get("snmp.host") !== typeof "string") {
+            throw new Error("snmp.host must be a string")
+        }
+
+        this.session = snmp.createSession(this.config.get("snmp.host"), this.config.get("snmp.community") ?? "public")
     }
     get(oid: string, cb: (error: any, varbinds: any) => void) {
         return this.session.get([oid], cb)
